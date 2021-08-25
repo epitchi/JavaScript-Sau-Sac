dfaab42f1bcca1ce732c9d5d8c4cca4b00f212fd
epitchi <thienvanlea1@gmail.com>
2021-08-25 08:39:04 +0700

    Variables and Assignment

---
+++ lesson-2/js/app.js
@@ -0,0 +1,24 @@
+var courseName = 'JS Sau Sac';
+
+// ES2015 Support
+let courseLength = 20;
+
+const MAX_NUMBER_OF_USER = 100;
+
+
+var courseName = 'JS Sau Sac Update';
+
+// let  courseLength = 10; cant switch scope, const same
+
+courseLength = 10;
+
+// MAX_NUMBER_OF_USER = 500; cant change value  
+
+// {
+//     let courseLength = 50;
+//     console.log(courseLength);
+// }
+
+
+console.log(courseName);
+
